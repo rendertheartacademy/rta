3f1440b962bb0dd3c23f56e5a74cc73384513f8d
rendertheartacademy <rendertheartacademy@gmail.com>
2026-01-11 04:22:24 +0700

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 // These environment variables must be set in your .env file or deployment settings
-const supabaseUrl = "https://ojduzzmiyihgeuzoiynm.supabase.co" 
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9qZHV6em1peWloZ2V1em9peW5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4NzI4NjAsImV4cCI6MjA4MzQ0ODg2MH0.d75bRPq_dxPAAIEY0KmIEAgjbaB5-wPp1HLVhuiX2MY"
+const supabaseUrl = "https://tckxjyqtifbvkucjtiuk.supabase.co" 
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja3hqeXF0aWZidmt1Y2p0aXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNjU0NzIsImV4cCI6MjA4MzY0MTQ3Mn0.db1750isqZguYlM-p8V8nPSVJyUOqnKQsQxwWJ_ZSwU"
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
